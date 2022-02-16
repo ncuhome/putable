4ff2da0255c88329e7f0edfe2eea47291f8cf581
nspyf <316851756@qq.com>
2022-02-16 21:36:46 +0800

    feat: complete storage

--- src/lib/storage/storage.ts
+++ src/lib/storage/storage.ts
@@ -1,14 +1,30 @@
-class Storage {
-  get<T>(key: string): T | null {
-    const data = localStorage.getItem(key)
+class Storage<T> {
+  private readonly prefix: string
+
+  constructor(prefix: string) {
+    this.prefix = prefix
+  }
+
+  private getFullKey(key: T): string {
+    return this.prefix + ':' + key
+  }
+
+  get<U>(key: T): U | null {
+    const fullKey = this.getFullKey(key)
+    const data = localStorage.getItem(fullKey)
     if(data !== null) return JSON.parse(data)
     return data
   }
-  set(key: string, data: any) {
-    localStorage.setItem(key, JSON.stringify(data))
+  set(key: T, data: any) {
+    const fullKey = this.getFullKey(key)
+    localStorage.setItem(fullKey, JSON.stringify(data))
   }
 }
 
-const storage = new Storage()
+const enum apiKey {
+  'apiData'
+}
+type apiKeyType = keyof typeof apiKey
+const storage = new Storage<apiKeyType>('api')
 
 export {storage};
