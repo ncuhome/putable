8986a05ba0f5d1f6e503517ddd1710bb79bf5630
WOo0W <24245520+WOo0W@users.noreply.github.com>
2022-03-15 18:51:14 +0800

    feat: 📰xlsx rich text support

--- src/lib/excel.ts
+++ src/lib/excel.ts
@@ -1,4 +1,4 @@
-import { Workbook } from 'exceljs'
+import { CellRichTextValue, CellValue, Workbook } from 'exceljs'
 import fileDownload from 'js-file-download'
 
 export const rowsToExcel = async (rows: string[][]) => {
@@ -6,10 +6,39 @@ export const rowsToExcel = async (rows: string[][]) => {
   const sh = wb.addWorksheet('Sheet1')
   sh.addRows(rows)
   const b = await wb.xlsx.writeBuffer()
-  // const dec = new TextDecoder('utf-8')
-  // console.log(dec.decode(b))
   fileDownload(b, new Date().toLocaleString() + '.xlsx')
 }
+
+const valueToString = (value: CellValue): string => {
+  if (!value) {
+    return ''
+  }
+  if (typeof value === 'string') {
+    return value
+  }
+  if (typeof value === 'number' || typeof value === 'boolean') {
+    return value.toString()
+  }
+  if (value instanceof Date) {
+    return value.toLocaleString()
+  }
+  if (typeof value === 'object') {
+    if ('error' in value) {
+      return value.error
+    }
+    if ('richText' in value) {
+      return value.richText.map((v) => v.text).join('')
+    }
+    if ('hyperlink' in value) {
+      // `value.hyperlink.text` can actually be string or CellRichTextValue
+      return `${valueToString(value.text as string | CellRichTextValue)} ${
+        value.hyperlink
+      }`
+    }
+  }
+  return ''
+}
+
 export const parseExcel = async (file: File): Promise<string[][]> => {
   const wb = new Workbook()
   const b = await file.arrayBuffer()
@@ -18,9 +47,10 @@ export const parseExcel = async (file: File): Promise<string[][]> => {
   const r: string[][] = []
   wb.worksheets[0].eachRow((row) => {
     if (Array.isArray(row.values)) {
-      r.push(row.values.slice(1).map((v) => v?.toString() || ''))
+      // Skip the first empty value in `row.values`
+      r.push(row.values.slice(1).map(valueToString))
     } else {
-      r.push(Object.values(row.values).map((v) => v?.toString() || ''))
+      r.push(Object.values(row.values).map(valueToString))
     }
   })
   return r
